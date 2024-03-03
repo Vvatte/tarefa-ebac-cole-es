2fb94fb9e88e458680a95aada57c042329288162
Vvatte <vvatte@yahoo.com>
2024-03-03 18:41:16 -0300

    resolvendo a primeira parte

--- colecoes/src/Tarefa1.java
+++ colecoes/src/Tarefa1.java
@@ -1,16 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+
 public class Tarefa1 {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Digiter três nomes separados por virgulas");
 
+        String frase = s.nextLine();
+        List lista = new ArrayList<String>(List.of(frase.split(", ")));
 
-        String nomes = s.next();
-        System.out.println(nomes);
-
-        String[] array = s.nextLine().split(",");
-        System.out.println("nomes:" + array);
+        System.out.println("Nomes digitados: "+ lista);
+        Collections.sort(lista);
+        System.out.println("Nomes digitados em ordem alfabética: " +lista);
 
     }
 }
